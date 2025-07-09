a9ed85e2299015788dc4e50a5220a386761febba
Y M Horn <ymh53r@gmail.com>
2025-07-09 16:10:12 +0300

    split up functions

--- BasicClassifier.py
+++ BasicClassifier.py
@@ -1,8 +1,5 @@
-from typing import final
-
 import pandas as pd
 from pprint import pprint
-
 
 # computer_buyers = pd.read_csv('computer_customers.csv',index_col='id')
 # computer_buyers.sort_index(inplace=True)
@@ -11,38 +8,63 @@ from pprint import pprint
 
 class NaiveBayes:
     def __init__(self,path,classifier,drop_columns):
-        self.path = path
         self.classifier = classifier
-        self.DB = pd.read_csv(self.path)
-        self.DB.drop(drop_columns,axis=1,inplace=True)
+        self.DF = pd.read_csv(path)
+        self.DF.drop(drop_columns,axis=1,inplace=True)
+
+    @staticmethod
+    def unique_values(dataframe,column):
+        return dataframe[column].unique()
+
+    @staticmethod
+    def amount_of_unique_values(dataframe,column,value):
+        return float(dataframe.loc[dataframe[column] == value,column].count())
+
+    @staticmethod
+    def amount_of_all_values(dataframe):
+        return float(dataframe.shape[0])
+
+    def split_dataframe_by_value(self,column,value):
+        return self.DF.groupby(column).get_group(value)
+
+    def columns_to_work_on(self,classifier):
+        columns = self.DF.columns
+        return columns.drop(classifier)
+
+    def sample(self,fraction):
+        return self.DF.sample(frac=fraction)
+
+    def left_sample(self,sample):
+        return self.DF[~self.DF.index.isin(sample.index)]
 
     def create_dict_class(self):
         class_dict = {}
-        unique_class = self.DB[self.classifier].unique()
-        for val in unique_class:
-            class_dict[val] = float(self.DB.loc[self.DB[self.classifier] == val,self.classifier].count() / self.DB[self.classifier].count())
+        all_values = self.amount_of_all_values(self.DF)
+        for val in self.unique_values(self.DF,self.classifier):
+            unique_values = self.amount_of_unique_values(self.DF,self.classifier,val)
+            class_dict[val] = unique_values / all_values
         return class_dict
 
     def create_dict_values(self):
-        unique_class = self.DB[self.classifier].unique()
         dict1 = {}
-        for val in unique_class:
-            split_df = self.DB.groupby(self.classifier).get_group(val)
-            column = self.DB.columns
-            column = column.drop(self.classifier)
+        for val in self.unique_values(self.DF,self.classifier):
+            split_df = self.split_dataframe_by_value(self.classifier,val)
+            column = self.columns_to_work_on(self.classifier)
             dict2 = {}
             for col in column:
-                count_val = split_df[self.classifier].count()
-                unique_val = self.DB[col].unique()
-                split_unique_val = split_df[col].unique()
+                count_val = self.amount_of_all_values(split_df)
+                unique_val = self.unique_values(self.DF,col)
+                split_unique_val = self.unique_values(split_df,col)
                 dict3 = {}
                 if len(unique_val) == len(split_unique_val):
                     for value in unique_val:
-                        dict3[value] = float(split_df.loc[split_df[col] == value,col].count() / count_val)
+                        value_count = self.amount_of_unique_values(split_df,col,value)
+                        dict3[value] = value_count / count_val
                 else:
                     count_val += len(unique_val)
                     for value in unique_val:
-                        dict3[value] = float((split_df.loc[split_df[col] == value, col].count() +1) / count_val)
+                        value_count = self.amount_of_unique_values(split_df, col, value) + 1
+                        dict3[value] = value_count / count_val
                 dict2[col] = dict3
             dict1[val] = dict2
         return dict1
@@ -70,10 +92,16 @@ class NaiveBayes:
         print(final_dict)
         return max(final_dict,key=final_dict.get)
 
+    def test(self):
+        sample = self.sample(0.7)
+        to_sample_with = self.left_sample(sample)
+
+
+
 
 
 
 
 a = NaiveBayes('computer_customers.csv','Buy_Computer','id')
-pprint(a.result(a.input()))
+pprint(a.test())
 
