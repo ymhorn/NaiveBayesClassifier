81f45f87bac875d7f06248e84165512647be8be3
Y M Horn <ymh53r@gmail.com>
2025-07-09 10:37:18 +0300

    Finished Dictionary and started working on Quistion

--- BasicClassifier.py
+++ BasicClassifier.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pprint import pprint
 
 
 # computer_buyers = pd.read_csv('computer_customers.csv',index_col='id')
@@ -7,10 +8,11 @@ import pandas as pd
 # print(split)
 
 class NaiveBayes:
-    def __init__(self,path,classifier):
+    def __init__(self,path,classifier,drop_columns):
         self.path = path
         self.classifier = classifier
         self.DB = pd.read_csv(self.path)
+        self.DB.drop(drop_columns,axis=1,inplace=True)
 
     def create_dict_class(self):
         class_dict = {}
@@ -20,25 +22,36 @@ class NaiveBayes:
         return class_dict
 
     def create_dict_values(self):
-        dict1 = {}
         unique_class = self.DB[self.classifier].unique()
+        dict1 = {}
         for val in unique_class:
             split_df = self.DB.groupby(self.classifier).get_group(val)
-            count_val = split_df[self.classifier].count()
+            column = self.DB.columns
+            column = column.drop(self.classifier)
             dict2 = {}
-            column = self.DB.columns[1:]
             for col in column:
+                count_val = split_df[self.classifier].count()
                 unique_val = self.DB[col].unique()
+                split_unique_val = split_df[col].unique()
                 dict3 = {}
-                for value in unique_val:
-                    dict3[value] = float(split_df.loc[split_df[col] == value,col].count() / count_val)
+                if len(unique_val) == len(split_unique_val):
+                    for value in unique_val:
+                        dict3[value] = float(split_df.loc[split_df[col] == value,col].count() / count_val)
+                else:
+                    count_val += len(unique_val)
+                    for value in unique_val:
+                        dict3[value] = float((split_df.loc[split_df[col] == value, col].count() +1) / count_val)
                 dict2[col] = dict3
             dict1[val] = dict2
         return dict1
 
+    def probability(self):
+        for k,v in self.create_dict_values().items():
+            for key,value in v.items():
+                lst = list(value.keys())
+                return lst
 
 
 
-
-a = NaiveBayes('computer_customers.csv','Buy_Computer')
-print(a.create_dict_values())
+a = NaiveBayes('computer_customers.csv','Buy_Computer','id')
+pprint(a.probability())
